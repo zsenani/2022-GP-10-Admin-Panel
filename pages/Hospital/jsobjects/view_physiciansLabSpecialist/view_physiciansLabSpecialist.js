73f692cfc04932ba377a2978224b1675f6a716a6
Medcore App <medcore.gp@gmail.com>
2023-04-06 00:45:38 +0000

    Fix bugs

--- pages/Hospital/jsobjects/view_physiciansLabSpecialist/view_physiciansLabSpecialist.js
+++ pages/Hospital/jsobjects/view_physiciansLabSpecialist/view_physiciansLabSpecialist.js
@@ -1,11 +1,7 @@
 export default {
 	viewPhysiciansLabSpecialist: async () => {
-		
 		showModal('ModalP')
 		await get_physician.run()
-		await get_physicianDetails.run()
 		await get_labSpecialist.run()
-		await get_labSpecialistDetails.run()
-		
 	}
 }
